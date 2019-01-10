39047f5bdf9438275ff100fb8861f0cd70b71aea
Thomas Phillips <tphillips@snap.com>
2019-01-09 16:45:27 -0800

    Added cypress script

---
+++ cypress/integration/input-form.spec.js
@@ -0,0 +1,7 @@
+describe('Input form', () => {
+    it('focuses input on load', () => {
+        cy.visit('http://localhost:3030');
+
+        cy.focused().should('have.class', 'new-todo');
+    });
+});
