64238862d196d3d0d1a0754d25c651646a6ad4e2
Thomas Phillips <tphillips@snap.com>
2019-01-10 11:55:08 -0800

    Test that todos load when the page reloads

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,30 +1,7 @@
-const todos = [
-  {
-    id: 1,
-    name: "F Bs",
-    isComplete: false
-  },
-  {
-    id: 2,
-    name: "Get money",
-    isComplete: false
-  },
-  {
-    id: 3,
-    name: "Stack Bread",
-    isComplete: false
-  },
-  {
-    id: 4,
-    name: "Make donuts",
-    isComplete: false
-  }
-];
-
 describe("App initialization", () => {
   it.only("Loads todos on page load", () => {
     cy.server();
-    cy.route('GET', '/api/todos', todos);
+    cy.route('GET', '/api/todos', 'fixture:todos');
     cy.visit('/');
 
     cy.get('.todo-list li')
