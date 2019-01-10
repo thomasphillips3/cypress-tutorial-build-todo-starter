3258caf4ca0e81b1f653d6436c423e7b937e48d3
Thomas Phillips <tphillips@snap.com>
2019-01-09 16:57:16 -0800

    Test: input form accepts input

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -4,4 +4,11 @@ describe('Input form', () => {
 
         cy.focused().should('have.class', 'new-todo');
     });
+
+    it.only('accepts input', () => {
+        const testText = 'smack fools';
+        cy.visit('http://localhost:3030');
+
+        cy.get('.new-todo').type(testText).should('have.value', testText);
+    });
 });
